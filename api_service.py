2a80ae156e04bed4a213f47d217d1d026199ccf1
Ivan <ilitteri@fi.uba.ar>
2021-09-20 13:31:13 -0300

    Add: google credentials & service files

---
+++ api_service.py
@@ -0,0 +1,13 @@
+from api.google_credentials import GoogleCredentials
+from googleapiclient.discovery import Resource, build
+
+
+class APIService(object):
+    __service: Resource
+
+    def __init__(self, service_name: str, api_version: str, google_credentials: GoogleCredentials) -> None:
+        self.__service = build(service_name, api_version,
+                               credentials=google_credentials.credentials())
+
+    def service(self) -> Resource:
+        return self.__service
